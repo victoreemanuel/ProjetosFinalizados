fb50020d85399f9680e416d350a067f0d95e8de0
Victor <victoremanuelvm.31@hotmail.com>
2025-09-22 18:39:56 -0300

    Remove arquivos Java que não pertencem ao frontend

--- SWA - Gestão de pessoas/Frontend/src/main/java/com/example/GestaoPessoas/Configuration/WebMvcConfigurer.java
+++
@@ -1,8 +0,0 @@
-package com.example.GestaoPessoas.Configuration;
-
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-public interface WebMvcConfigurer {
-
-    void addCorsMappings(CorsRegistry registry);
-}
--- SWA - Gestão de pessoas/Frontend/src/main/java/com/example/GestaoPessoas/Repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.GestaoPessoas.Repository;
-
-import com.example.GestaoPessoas.Model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
